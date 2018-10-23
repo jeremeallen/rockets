fa9ecee2efcf701dddbb2fd3bc0fb1e483c9275e
Jereme Allen <jallen@allensservices.com>
2018-10-22 21:17:51 -0500

    Add in Google sheets as the data source

--- src/components/Roster.vue
+++ src/components/Roster.vue
@@ -1,88 +1,50 @@
 <template>
   <ul class="list-group">
-    <li class="list-group-item">
-      <h3>MICK HENEGHAN
-      <span class="badge badge-primary badge-pill float-right">2</span></h3>
-    </li>  
-    <li class="list-group-item">
-      <h3>MARK AMATETTI
-      <span class="badge badge-primary badge-pill float-right">4</span></h3>
+    <li class="list-group-item" v-for='(player, index) in orderedPlayers' :key='`player-${ index }`'>
+      <h3>{{ player.firstName }} {{ player.lastName }}
+      <span class="badge badge-primary badge-pill float-right">{{ player.number }}</span></h3>
     </li> 
-    <li class="list-group-item">
-      <h3>JOHN KUHL
-      <span class="badge badge-primary badge-pill float-right">5</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>ROBBY HAAK
-      <span class="badge badge-primary badge-pill float-right">8</span></h3>
-    </li>  
-    <li class="list-group-item">
-      <h3>BRADLEY CONNOR
-      <span class="badge badge-primary badge-pill float-right">12</span></h3>
-    </li> 
-    <li class="list-group-item">
-      <h3>TYLER POHLIG
-      <span class="badge badge-primary badge-pill float-right">15</span></h3>
-    </li>  
-    <li class="list-group-item">
-      <h3>RYAN SORKIN
-      <span class="badge badge-primary badge-pill float-right">17</span></h3>
-    </li>  
-    <li class="list-group-item">
-      <h3>CASEY SOTANELLA
-      <span class="badge badge-primary badge-pill float-right">18</span></h3>
-    </li> 
-    <li class="list-group-item">
-      <h3>LUKE HOUSE
-      <span class="badge badge-primary badge-pill float-right">19</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>PAUL BRYER
-      <span class="badge badge-primary badge-pill float-right">27</span></h3>
-    </li> 
-    <li class="list-group-item">
-      <h3>JOHN D'ANIELLO
-      <span class="badge badge-primary badge-pill float-right">28</span></h3>
-    </li> 
-    <li class="list-group-item">
-      <h3>MITCH RICKERT
-      <span class="badge badge-primary badge-pill float-right">29</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>ILYA FEDEROV
-      <span class="badge badge-primary badge-pill float-right">52</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>JESSE LOWELL
-      <span class="badge badge-primary badge-pill float-right">61</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>TROY CROSSON
-      <span class="badge badge-primary badge-pill float-right">68</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>VLAD SAIKO
-      <span class="badge badge-primary badge-pill float-right">70</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>ELLIOTT BURGIN
-      <span class="badge badge-primary badge-pill float-right">77</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>TUCKER TYNAN
-      <span class="badge badge-primary badge-pill float-right">82</span></h3>
-    </li>
-    <li class="list-group-item">
-      <h3>TRAVIS ALLEN
-      <span class="badge badge-primary badge-pill float-right">84</span></h3>
-    </li>
   </ul>
 </template>
 
 <script>
+import axios from 'axios';
+import _ from 'lodash';
 
 export default {
   name: 'Roster',
+  data() {
+    return {
+      headers: ['First Name', 'Last Name', 'Number'],
+      players: [],
+    };
+  },
+  computed: {
+    cleanedPlayers() {
+      return this.players.map(p => {
+        return {
+          firstName: p.gsx$firstname['$t'],
+          lastName: p.gsx$lastname['$t'],
+          number: Number(p.gsx$number['$t']),
+        };
+      });
+    },
+
+    orderedPlayers() {
+      return _.sortBy(this.cleanedPlayers, [function(p) { return p.number; }]);
+    }
+  },
+  methods: {
+    getRoster() {
+      axios.get('https://spreadsheets.google.com/feeds/list/1FB7Gn0Ydtbmv2C2iNMjDm37FGAj-7jIotma7vr6qXzI/od6/public/values?alt=json')
+        .then(response => {
+          this.players = response.data.feed.entry;
+        })
+    },
+  },
+  created() {
+    this.getRoster();
+  },
 }
 </script>
 
